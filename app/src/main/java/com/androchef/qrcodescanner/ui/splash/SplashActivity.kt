e59eb8e9a5efe37dd1346c88ea79ff4aae0243a1
Happy Singh <happysingh23828@gmail.com>
2019-07-06 19:53:08 +0530

    Permissions Handled

--- app/src/main/java/com/androchef/qrcodescanner/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/androchef/qrcodescanner/ui/splash/SplashActivity.kt
@@ -1,10 +1,17 @@
 package com.androchef.qrcodescanner.ui.splash
 
+import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import com.androchef.qrcodescanner.ui.mainactivity.MainActivity
 import com.androchef.qrcodescanner.R
@@ -18,12 +25,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        checkForPermission()
+        Handler().postDelayed({
+            checkForPermission()
+        }, 2000)
     }
 
     private fun checkForPermission() {
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
-           gotToMainActivity()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            gotToMainActivity()
         } else {
             requestPermission()
         }
@@ -31,25 +40,64 @@ class SplashActivity : AppCompatActivity() {
 
 
     private fun requestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),
-            CAMERA_PERMISSION_REQUEST_CODE)
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
     }
 
     private fun gotToMainActivity() {
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        },3000)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == CAMERA_PERMISSION_REQUEST_CODE){
-            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 gotToMainActivity()
+            } else if (isPermanentlyDenied()) {
+                showGoToAppSettingsDialog()
             } else
                 requestPermission()
         }
     }
 
+    private fun isPermanentlyDenied(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA).not()
+        } else {
+            return false
+        }
+    }
+
+
+    private fun showGoToAppSettingsDialog() {
+        AlertDialog.Builder(this, R.style.CustomAlertDialog)
+            .setTitle(getString(R.string.grant_permissions))
+            .setMessage(getString(R.string.we_need_permission))
+            .setPositiveButton(getString(R.string.grant)) { _, _ ->
+                goToAppSettings()
+            }
+            .setNegativeButton(getString(R.string.cancel)) { _, _ ->
+                run {
+                    finish()
+                }
+            }.show()
+    }
+
+    private fun goToAppSettings() {
+        val intent = Intent(
+            ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        checkForPermission()
+    }
 }
